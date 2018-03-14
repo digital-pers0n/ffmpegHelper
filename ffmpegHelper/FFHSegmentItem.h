7c906a64ea5ec6d5661dabbe768445a4ae6eaa0c
digital-pers0n <aquaphonic@gmail.com>
2018-03-14 21:37:38 +0300

    Adding SegmentItem Class

---
+++ ffmpegHelper/FFHSegmentItem.h
@@ -0,0 +1,16 @@
+//
+//  FFHSegmentItem.h
+//  ffmpegHelper
+//
+//  Created by Terminator on 2018/03/12.
+//  Copyright © 2018年 Terminator. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFHSegmentItem : NSObject
+
+@property float startTime;
+@property float endTime;
+
+@end
---
+++ ffmpegHelper/FFHSegmentItem.m
@@ -0,0 +1,13 @@
+//
+//  FFHSegmentItem.m
+//  ffmpegHelper
+//
+//  Created by Terminator on 2018/03/12.
+//  Copyright © 2018年 Terminator. All rights reserved.
+//
+
+#import "FFHSegmentItem.h"
+
+@implementation FFHSegmentItem
+
+@end
